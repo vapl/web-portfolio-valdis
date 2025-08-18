685cb37771f5baecc7d04815835bf212f39aebeb
Valdis Vaščenkovs <valdis.vascenkovs@gmail.com>
2025-08-18 17:20:18 +0300

    Modifications

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,7 @@ export default function Home() {
   const projects = getAllProjects();
   return (
     <main>
-<<<<<<< HEAD
-      <HeroSlideshow segmentMs={6000} />
-=======
-      <Navigation />
       <HeroSlideshow segmentMs={6000} projects={projects} />
->>>>>>> 8b1cb7d260b0a6a5cef5326f1cde4d85de5d5394
     </main>
   );
 }
