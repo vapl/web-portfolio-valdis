95493651a08966df92bd8921576e8fb25a4aa555
Valdis Vaščenkovs <valdis.vascenkovs@gmail.com>
2025-08-21 11:18:33 +0300

    Prepared about page; Fixed Contact page 'About' link

---
+++ src/app/about/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutPage = () => {
+  return (
+    <div className='flex w-full min-h-screen items-center justify-center '>
+        <h1 className=''>About Page</h1>
+    </div>
+  )
+}
+
+export default aboutPage
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -13,62 +13,62 @@ export default function Navigation() {
     <>
       {/* About (left) */}
       <Link
-          href={pathName === "about" ? "" : "/about"}
-        >
-      <div 
-        className="fixed left-0 w-8 md:w-11 top-1/2 -translate-y-1/2 flex justify-end cursor-pointer z-[999]"
-        onMouseEnter={() => setAboutHovered(true)}
-        onMouseLeave={() => setAboutHovered(false)}
+        href={pathName === "/about" ? "" : "/about"}
       >
-        <div
-          className={`group flex flex-col items-center tracking-[0.25em] text-text/80`}
+        <div 
+          className={`fixed left-0 w-8 md:w-11 top-1/2 -translate-y-1/2 flex justify-end ${AboutHovered && pathName === "/about" ? "cursor-auto" : "cursor-pointer"} z-[999]`}
+          onMouseEnter={() => setAboutHovered(true)}
+          onMouseLeave={() => setAboutHovered(false)}
         >
-          {"ABOUT".split("").map((ch, i) => (
-            <span
-              key={i}
-              className={`transition-all duration-300 ${
-                AboutHovered || pathName === "/about"  ? "text-[#d97706] font-extrabold" : "text-text/80"
-              }`}
-              style={{
-                transitionDelay: AboutHovered ? `${i * 50}ms` : "0ms",
-                marginTop: AboutHovered || pathName === "about" && i !== 0 ? "0.25rem" : "0.15rem",
-              }}
-            >
-              {ch}
-            </span>
-          ))}
+          <div
+            className={`group flex flex-col items-center tracking-[0.25em] text-text/80`}
+          >
+            {"ABOUT".split("").map((ch, i) => (
+              <span
+                key={i}
+                className={`transition-all duration-150 ${
+                  AboutHovered || pathName === "/about"  ? "text-[#d97706] font-extrabold" : "text-text/80"
+                }`}
+                style={{
+                  transitionDelay: AboutHovered ? `${i * 50}ms` : "0ms",
+                  marginTop: (AboutHovered || pathName === "/about") && i !== 0 ? "0.25rem" : "0.15rem",
+                }}
+              >
+                {ch}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
       </Link>
 
       {/* Contact (right) */}
       <Link
           href={ pathName === "/contact" ? "" : "/contact"}
         >
-      <div 
-        className={`fixed right-0 w-8 md:w-11 top-1/2 -translate-y-1/2 flex justify-start ${ContactsHovered && pathName === "/contact" ? "cursor-auto" : "cursor-pointer"} z-[999]`}
-        onMouseEnter={() => setContactsHovered(true)}
-        onMouseLeave={() => setContactsHovered(false)}
-      >
-        <div
-          className="group flex flex-col items-center text-text/80"
+        <div 
+          className={`fixed right-0 w-8 md:w-11 top-1/2 -translate-y-1/2 flex justify-start ${ContactsHovered && pathName === "/contact" ? "cursor-auto" : "cursor-pointer"} z-[999]`}
+          onMouseEnter={() => setContactsHovered(true)}
+          onMouseLeave={() => setContactsHovered(false)}
         >
-          {"CONTACT".split("").map((ch, i) => (
-            <span
-              key={i}
-              className={`transition-all duration-150 ${
-                ContactsHovered || pathName === "/contact" ? "text-[#d97706] font-extrabold" : "text-text/80"
-              }`}
-              style={{
-                transitionDelay: ContactsHovered ? `${i * 50}ms` : "0ms",
-                marginTop: (ContactsHovered || pathName === "/contact") && i !== 0 ? "0.25rem" : "0.15rem",
-              }}
-            >
-              {ch}
-            </span>
-          ))}
+          <div
+            className="group flex flex-col items-center text-text/80"
+          >
+            {"CONTACT".split("").map((ch, i) => (
+              <span
+                key={i}
+                className={`transition-all duration-150 ${
+                  ContactsHovered || pathName === "/contact" ? "text-[#d97706] font-extrabold" : "text-text/80"
+                }`}
+                style={{
+                  transitionDelay: ContactsHovered ? `${i * 50}ms` : "0ms",
+                  marginTop: (ContactsHovered || pathName === "/contact") && i !== 0 ? "0.25rem" : "0.15rem",
+                }}
+              >
+                {ch}
+              </span>
+            ))}
+          </div>
         </div>
-      </div>
       </Link>
     </>
   );
